21c13cedb897a9d56bf509d05a9d1f2faa433974
TahirMansuri <tprocks786@gmail.com>
2022-01-20 12:35:16 +0530

    MODIFIED: Count for Frequent Word in String

--- src/com/bridgelabz/HashTableDemo.java
+++ src/com/bridgelabz/HashTableDemo.java
@@ -9,5 +9,27 @@ public class HashTableDemo {
         //Welcome message for User
         System.out.println("Welcome to Hash Table and Binary Tree Problem Developed by, Tahir Mansuri.");
 
+        //Creating Linked List Object
+        LinkedList<String> linkedList = new LinkedList();
+        Scanner sc = new Scanner(System.in);
+
+        //Getting String Input from User
+        System.out.println("Enter the String : ");
+        String s = sc.nextLine();
+
+        //Split String into String Object Array
+        String a[] = s.split(" ");
+
+        //Insert each separated string word in Linked List from Last
+        for(String x : a) {
+            linkedList.insertLast(x);
+        }
+
+        //Showing the Linked List String
+        linkedList.showLinkedList();
+
+        //Show Frequent Words Count
+        linkedList.showFrequent();
+
     }
 }
