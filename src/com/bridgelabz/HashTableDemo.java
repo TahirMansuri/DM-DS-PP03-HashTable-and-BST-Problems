aec1684a5913391ce252b32eeeee795f4dfcb65a
TahirMansuri <tprocks786@gmail.com>
2022-01-20 12:32:27 +0530

    ADDED : Hash Table and Binary Tree Problem

---
+++ src/com/bridgelabz/HashTableDemo.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class HashTableDemo {
+    public static void main(String[] args) {
+        //Welcome message for User
+        System.out.println("Welcome to Hash Table and Binary Tree Problem Developed by, Tahir Mansuri.");
+
+    }
+}
